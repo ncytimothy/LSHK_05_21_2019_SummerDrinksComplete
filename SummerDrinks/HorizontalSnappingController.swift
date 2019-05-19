b8835884a5d25e203836d6509ba1a6089ffdd9d2
Tim Ng <ncy.timothy@gmail.com>
2019-05-19 15:33:03 +0800

    Second solution, but does not work entirely

--- SummerDrinks/HorizontalSnappingController.swift
+++ SummerDrinks/HorizontalSnappingController.swift
@@ -24,17 +24,40 @@ class HorizontalSnappingCollectionView: UICollectionView {
 
 class SnappingLayout: UICollectionViewFlowLayout {
     // snapping behavior, rudimentary solution
+    
     override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint, withScrollingVelocity velocity: CGPoint) -> CGPoint {
-        // .zero snaps back to (0,0)
-        guard let collectionView = self.collectionView else {
-            return super.targetContentOffset(forProposedContentOffset: proposedContentOffset, withScrollingVelocity: velocity)
-        }
-        let horizontalOffset: CGFloat = 48
-        // Rough math
-        let itemWidth = collectionView.frame.width - horizontalOffset
-        let pageNumber = collectionView.contentOffset.x / itemWidth
-        print(round(pageNumber))
+        guard let collectionView = collectionView else { return super.targetContentOffset(forProposedContentOffset: proposedContentOffset, withScrollingVelocity: velocity) }
+        let parent = super.targetContentOffset(forProposedContentOffset: proposedContentOffset, withScrollingVelocity: velocity)
         
-         return CGPoint(x: pageNumber * itemWidth, y: 0)
+        let itemWidth = collectionView.frame.width - 48
+        let itemSpace = itemWidth + minimumInteritemSpacing
+        var currentItemIdx = round(collectionView.contentOffset.x / itemSpace)
+        
+        // Skip to the next cell, if there is residual scrolling velocity left.
+        // This helps to prevent glitches
+        let vX = velocity.x
+        if vX > 0 {
+            currentItemIdx += 1
+        } else if vX < 0 {
+            currentItemIdx -= 1
+        }
+        
+        let nearestPageOffset = currentItemIdx * itemSpace
+        return CGPoint(x: nearestPageOffset,
+                       y: parent.y)
+    
+//    override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint, withScrollingVelocity velocity: CGPoint) -> CGPoint {
+//        // .zero snaps back to (0,0)
+//        guard let collectionView = self.collectionView else {
+//            return super.targetContentOffset(forProposedContentOffset: proposedContentOffset, withScrollingVelocity: velocity)
+//        }
+//        let horizontalOffset: CGFloat = 48
+//        // Rough math
+//        let itemWidth = collectionView.frame.width - horizontalOffset
+//        let pageNumber = collectionView.contentOffset.x / itemWidth
+//        print(round(pageNumber))
+//
+//         return CGPoint(x: pageNumber * itemWidth, y: 0)
+//    }
     }
 }
